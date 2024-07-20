03b15ae1ffd30373ce31da355775d29d8b55928a
Serghini <abdelmoulaserghini@gmail.com>
2024-07-20 13:22:07 +0100

    skills section

---
+++ src/components/skills/Tools.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+const Tools = () => {
+  return (
+    <div className="skills__content">
+      <h3 className="skills__title">Tools&Methodes</h3>
+
+      <div className="skills__box">
+        <div className="skills__group">
+
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills__name">GIT&Github</h3>
+              <span className="skills__level">Advanced</span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills__name">Scrum</h3>
+              <span className="skills__level">Intermediate</span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills__name">Docker</h3>
+              <span className="skills__level">Basic</span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills__name">POO</h3>
+              <span className="skills__level">Intermediate</span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills__name">Vercel</h3>
+              <span className="skills__level">Intermediate</span>
+            </div>
+          </div>
+
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Tools;
